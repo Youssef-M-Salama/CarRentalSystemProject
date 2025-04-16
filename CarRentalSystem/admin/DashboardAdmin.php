9c9aa72e949a5d07eb7fd29d5906383cc5d54102
Youssef-AbuAbdallah <youssefisjo123@gmail.com>
2025-04-17 00:06:59 +0200

    Fix the CSS bug in the Dashboard admin

--- CarRentalSystem/admin/DashboardAdmin.php
+++ CarRentalSystem/admin/DashboardAdmin.php
@@ -87,7 +87,11 @@ $unavailable_cars = mysqli_query($conn, $query);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard</title>
-    <link rel="stylesheet" href="../styles.css">
+    
+    <link rel="stylesheet" href="../css/general.css">
+    <link rel="stylesheet" href="../css/header.css">
+    <link rel="stylesheet" href="../css/main-content.css">
+
     <style>
         /* Tab Navigation Styles */
         .tab-navigation {
